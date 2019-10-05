9817232ee7d14409c17d57df54dd6cdb763076bd
Joesh Bautista <joeshbautista@csu.fullerton.edu>
2019-10-05 00:28:55 -0700

    Initial commit

---
+++ app/src/main/java/com/example/hw1/LinearActivity.java
@@ -0,0 +1,14 @@
+package com.example.hw1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LinearActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.linear_layout);
+        //setContentView(R.layout.linear_layout);
+    }
+}
