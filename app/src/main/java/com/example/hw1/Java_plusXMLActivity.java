11da90463edbde9b6bc644e26939cea06db951e8
Joesh Bautista <joeshbautista@csu.fullerton.edu>
2019-10-07 00:31:12 -0700

    Scrollview updates

--- app/src/main/java/com/example/hw1/Java_plusXMLActivity.java
+++ app/src/main/java/com/example/hw1/Java_plusXMLActivity.java
@@ -2,8 +2,8 @@ package com.example.hw1;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -25,7 +25,7 @@ public class Java_plusXMLActivity extends Activity{
         );
 
         //This is the row of numbers on the left side of the screen
-        for (int i=1; i <= 11; i++){
+        for (int i = 1; i <= 11; i++){
             TextView tv = new TextView(this);
             tv.setLayoutParams(tvParams);
             tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
@@ -34,41 +34,51 @@ public class Java_plusXMLActivity extends Activity{
             tv.setBackgroundColor(Color.GRAY);
         }
 
-        // This is for row 6 of the table
-        lv = (LinearLayout) findViewById(R.id.row6linearLayout);
+        //BEGIN line 4
+        lv = (LinearLayout) findViewById(R.id.row4);
 
 
-        TextView tView= new TextView(this);
+        TextView tv = new TextView(this);
         LinearLayout.LayoutParams textParam = new LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT, //width
                 ViewGroup.LayoutParams.WRAP_CONTENT //height
         );
-        textParam.weight = 1;
-        tView.setBackgroundColor(Color.CYAN);
-        tView.setText("            ");
-        tView.setTextAlignment(ViewGroup.TEXT_ALIGNMENT_CENTER);
-        lv.addView(tView, textParam);
+        tv.setText("Rule");
+        tv.setPadding(100,0,70,0);
+        tv.setTypeface(null, Typeface.BOLD);
+        lv.addView(tv, textParam);
 
-        textParam.weight = .5f;
-        tView= new TextView(this);
-        tView.setBackgroundColor(Color.CYAN);
-        tView.setText("int min");
-        tView.setTextAlignment(ViewGroup.TEXT_ALIGNMENT_CENTER);
-        lv.addView(tView, textParam);
+        tv = new TextView(this);
+        tv.setText("C1");
+        tv.setPadding(300,0,200,0);
+        tv.setTypeface(null, Typeface.BOLD);
+        lv.addView(tv, textParam);
 
-        textParam.weight = .5f;
-        tView= new TextView(this);
-        tView.setBackgroundColor(Color.CYAN);
-        tView.setText("   int max");
-        tView.setTextAlignment(ViewGroup.TEXT_ALIGNMENT_CENTER);
-        lv.addView(tView, textParam);
+        tv = new TextView(this);
+        tv.setText("A1");
+        tv.setPadding(300,0,200,0);
+        tv.setTypeface(null, Typeface.BOLD);
+        lv.addView(tv, textParam);
+        //END line 4
 
-        textParam.weight = 1;
-        tView= new TextView(this);
-        tView.setBackgroundColor(Color.CYAN);
-        tView.setText("String greeting");
-        tView.setTextAlignment(ViewGroup.TEXT_ALIGNMENT_CENTER);
-        lv.addView(tView, textParam);
+        //BEGIN line 6
+        lv = (LinearLayout) findViewById(R.id.row6);
 
+
+        tv = new TextView(this);
+        //tv = (TextView) findViewById(R.id.tv61);
+        tv.setText("int min");
+        tv.setPadding(70,0,70,0);
+        lv.addView(tv, textParam);
+
+        tv = new TextView(this);
+        tv.setText("int max");
+        tv.setPadding(55,0,55,0);
+        lv.addView(tv, textParam);
+
+        tv = new TextView(this);
+        tv.setText("String greeting");
+        tv.setPadding(200,0,51,0);
+        lv.addView(tv, textParam);
     }
 }
